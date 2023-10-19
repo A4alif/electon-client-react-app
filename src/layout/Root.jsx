9223d7c3744e8eb70a57806b779fa6b5d25444c2
alifhasanshah <alifhasanshah@gmail.com>
2023-10-19 23:03:55 +0600

    footer added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const Root = () => {
   return (
@@ -11,6 +12,7 @@ const Root = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
